cbf0f624710e9fb564897f24c6a2e03f04c05ea5
Kevin Jiang <kevin51jiang@email.com>
2020-10-23 08:46:48 -0400

    main changes done

---
+++ src/components/sections/FeaturesSplit.js
@@ -0,0 +1,177 @@
+import React from "react";
+import classNames from "classnames";
+import { SectionSplitProps } from "../../utils/SectionProps";
+import SectionHeader from "./partials/SectionHeader";
+import Image from "../elements/Image";
+
+const propTypes = {
+  ...SectionSplitProps.types,
+};
+
+const defaultProps = {
+  ...SectionSplitProps.defaults,
+};
+
+const FeaturesSplit = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  invertMobile,
+  invertDesktop,
+  alignTop,
+  imageFill,
+  ...props
+}) => {
+  const outerClasses = classNames(
+    "features-split section",
+    topOuterDivider && "has-top-divider",
+    bottomOuterDivider && "has-bottom-divider",
+    hasBgColor && "has-bg-color",
+    invertColor && "invert-color",
+    className
+  );
+
+  const innerClasses = classNames(
+    "features-split-inner section-inner",
+    topDivider && "has-top-divider",
+    bottomDivider && "has-bottom-divider"
+  );
+
+  const splitClasses = classNames(
+    "split-wrap",
+    invertMobile && "invert-mobile",
+    invertDesktop && "invert-desktop",
+    alignTop && "align-top"
+  );
+
+  const sectionHeader = {
+    title: "Here's what I'll do",
+    paragraph:
+      "It's better and more useful to everyone if you fix the big problems first — all done as transparently as possible.",
+  };
+
+  return (
+    <section {...props} className={outerClasses}>
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={splitClasses}>
+            <div className="split-item">
+              <div
+                className="split-item-content center-content-mobile reveal-from-left"
+                data-reveal-container=".split-item"
+              >
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Emergency meeting
+                </div>
+                <h3 className="mt-0 mb-12">COVID-19 Recovery</h3>
+                <p className="m-0">
+                  <div>
+                    If the term ends up being <i>online</i>, the helping to
+                    advocate with profs and departments to create better courses
+                    in conditions that treat students better.
+                  </div>
+                  <div>
+                    If the term ends up being <i>offline</i>, then the
+                    transition to safe in person activities will require lots of
+                    attention.
+                  </div>
+                  <div>
+                    I will make sure that in either situation the transition
+                    will be a smooth as possible, allowing you to focus on what
+                    you actually paid for — an education.
+                  </div>
+                </p>
+              </div>
+              <div
+                className={classNames(
+                  "split-item-image center-content-mobile reveal-from-bottom",
+                  imageFill && "split-item-image-fill"
+                )}
+                data-reveal-container=".split-item"
+              >
+                <Image
+                  src={require("./../../assets/images/social-distancing.svg")}
+                  alt="Social distancing"
+                  width={528}
+                  height={396}
+                />
+              </div>
+            </div>
+            <div className="split-item">
+              <div
+                className="split-item-content center-content-mobile reveal-from-left"
+                data-reveal-container=".split-item"
+              >
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  The full adventure
+                </div>
+                <h3 className="mt-0 mb-12">First-year experience</h3>
+                <p className="m-0">
+                  First years feel like they've just been dropped in the deep
+                  end without support and robbed of their first-year experience.
+                  There needs to be at least enough support to ensure that they
+                  can actually float.
+                </p>
+              </div>
+              <div
+                className={classNames(
+                  "split-item-image center-content-mobile reveal-from-bottom",
+                  imageFill && "split-item-image-fill"
+                )}
+                data-reveal-container=".split-item"
+              >
+                <Image
+                  src={require("./../../assets/images/kick.gif")}
+                  alt="Mansour kicking chair"
+                  width={528}
+                  height={396}
+                />
+              </div>
+            </div>
+
+            <div className="split-item">
+              <div
+                className="split-item-content center-content-mobile reveal-from-right"
+                data-reveal-container=".split-item"
+              >
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Reviewing resumes and more
+                </div>
+                <h3 className="mt-0 mb-12">Strengthen widely used programs</h3>
+                <p className="m-0">
+                  Directing more resources and focus towards EngSoc's most
+                  widely used programs where the highest amount of people can
+                  benefit, like the resume critiques every term.
+                </p>
+              </div>
+              <div
+                className={classNames(
+                  "split-item-image center-content-mobile reveal-from-bottom",
+                  imageFill && "split-item-image-fill"
+                )}
+                data-reveal-container=".split-item"
+              >
+                <Image
+                  src={require("./../../assets/images/people.svg")}
+                  alt="People"
+                  width={528}
+                  height={396}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+FeaturesSplit.propTypes = propTypes;
+FeaturesSplit.defaultProps = defaultProps;
+
+export default FeaturesSplit;
---
+++ src/components/sections/Hero.js
@@ -0,0 +1,145 @@
+import React, { useState } from "react";
+import classNames from "classnames";
+import { SectionProps } from "../../utils/SectionProps";
+import ButtonGroup from "../elements/ButtonGroup";
+import Button from "../elements/Button";
+import Image from "../elements/Image";
+import Modal from "../elements/Modal";
+import Countdown from "react-countdown";
+
+const propTypes = {
+  ...SectionProps.types,
+};
+
+const defaultProps = {
+  ...SectionProps.defaults,
+};
+
+const countdownRender = ({days, hours, minutes, seconds, completed , formatted}) => {
+  if (completed) {
+    // Render a completed state
+
+    return (
+      <Button
+        tag="a"
+        color="dark"
+        wideMobile
+        href="https://vote.wusa.ca/ballots/210"
+      >
+        Time to vote! 🎉
+      </Button>
+    );
+  } else {
+    // Render a countdown
+    return (
+      <Button color="dark" wideMobile disabled>
+        🔒 {formatted.days}:{formatted.hours}:{formatted.minutes}:
+        {formatted.seconds}
+      </Button>
+    );
+  }
+};
+
+const Hero = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  ...props
+}) => {
+  const [videoModalActive, setVideomodalactive] = useState(false);
+
+  const openModal = (e) => {
+    e.preventDefault();
+    setVideomodalactive(true);
+  };
+
+  const closeModal = (e) => {
+    e.preventDefault();
+    setVideomodalactive(false);
+  };
+
+  const outerClasses = classNames(
+    "hero section center-content",
+    topOuterDivider && "has-top-divider",
+    bottomOuterDivider && "has-bottom-divider",
+    hasBgColor && "has-bg-color",
+    invertColor && "invert-color",
+    className
+  );
+
+  const innerClasses = classNames(
+    "hero-inner section-inner",
+    topDivider && "has-top-divider",
+    bottomDivider && "has-bottom-divider"
+  );
+
+  return (
+    <section {...props} className={outerClasses}>
+      <div className="container-sm">
+        <div className={innerClasses}>
+          <div className="hero-content">
+            <h1
+              className="mt-0 mb-16 reveal-from-bottom"
+              data-reveal-delay="200"
+            >
+              Kevin Jiang for{" "}
+              <span className="text-color-primary">VP Academic</span>
+            </h1>
+            <div className="container-xs">
+              <p
+                className="m-0 mb-32 reveal-from-bottom"
+                data-reveal-delay="400"
+              >
+                I don't know some witty thing should be here *insert witty
+                phrase here*
+              </p>
+              <div className="reveal-from-bottom" data-reveal-delay="600">
+                <ButtonGroup>
+                  <Button
+                    tag="a"
+                    color="primary"
+                    wideMobile
+                    href="#feature-tiles"
+                  >
+                    Get started
+                  </Button>
+
+                  <Countdown date={1604030400000} renderer={countdownRender} />
+                </ButtonGroup>
+              </div>
+            </div>
+          </div>
+          <div
+            className="hero-figure reveal-from-bottom illustration-element-01"
+            data-reveal-value="20px"
+            data-reveal-delay="800"
+          >
+            <Image
+              className="has-shadow"
+              src={require("./../../assets/images/video-placeholder.jpg")}
+              alt="Hero"
+              width={896}
+              height={504}
+            />
+          </div>
+          {/* <Modal
+            id="video-modal"
+            show={videoModalActive}
+            handleClose={closeModal}
+            video="https://player.vimeo.com/video/174002812"
+            videoTag="iframe"
+          /> */}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+Hero.propTypes = propTypes;
+Hero.defaultProps = defaultProps;
+
+export default Hero;
