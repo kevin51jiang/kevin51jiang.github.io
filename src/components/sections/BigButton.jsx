7f65ceccc6e0f464fb683723e44a1cf853191713
Kevin Jiang <kevin51jiang@email.com>
2020-10-28 03:56:44 -0400

    add sounds

--- src/components/sections/BigButton.jsx
+++ src/components/sections/BigButton.jsx
@@ -1,19 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, { createRef, useEffect, useRef, useState } from "react";
 import firebase from "firebase";
 import FlipNumbers from "react-flip-numbers";
 import { firebaseConfig } from "../../utils/config";
 import { useAudioPlayer } from "react-use-audio-player";
+import { Howl } from "howler";
 
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
+const availSounds = {
+  click: {
+    src: require("../../assets/sounds/click.mp3"),
+  },
+  get100: {
+    src: require("../../assets/sounds/get-100.mp3"),
+  },
+  get500: {
+    src: require("../../assets/sounds/get-500.mp3"),
+  },
+  get1000: {
+    src: require("../../assets/sounds/get-1000.mp3"),
+  },
+};
+
 const BigButton = (props) => {
   const [num, setNum] = useState(1234);
-  const { togglePlayPause, play, ready, loading, playing } = useAudioPlayer({
+  const { play, ready } = useAudioPlayer({
     src: require("../../assets/sounds/click.mp3"),
     format: "mp3",
     autoplay: false,
   });
+  const soundRef = useRef();
+
+  useEffect(() => {
+    soundRef.current = [];
+
+    Object.keys(availSounds).forEach((sound) => {
+      soundRef.current.push(
+        new Howl({ ...availSounds[sound], format: "mp3", autoplay: false })
+      );
+    });
+  }, []);
 
   useEffect(() => {
     firebase
@@ -26,14 +53,23 @@ const BigButton = (props) => {
       });
   });
 
+  // plays a bunch of sounds depending on number
+  useEffect(() => {
+    if (soundRef?.current) {
+      if (num % 100 === 0) {
+        soundRef.current[1].play();
+      } else if (num % 500 === 0) {
+        soundRef.current[2].play();
+      } else if (num % 1000 === 0) {
+        soundRef.current[3].play();
+      }
+    }
+  }, [num]);
+
   const onClick = () => {
     if (ready) {
       play();
     }
-    console.log("playing", playing);
-    console.log("loading", loading);
-    console.log("ready", ready);
-
     setNum(num + 1);
     firebase
       .database()
